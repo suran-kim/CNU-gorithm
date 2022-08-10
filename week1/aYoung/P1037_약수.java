5e24b2cadf2eb124d62e0bb5bf8f9278842d3549
Kim-AYoung <onionlily123@naver.com>
2022-08-10 10:35:02 +0900

    김아영: 백준 8393

--- week1/aYoung/P1037_약수.java
+++ week1/aYoung/P1037_약수.java
@@ -1,5 +1,3 @@
-package math;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Arrays;
--- week1/aYoung/P1929_소수구하기.java
+++ week1/aYoung/P1929_소수구하기.java
@@ -1,5 +1,3 @@
-package math;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
