deabda91976cd33efa9cefab35dccf63b265b835
H2W0N <woohw97@naver.com>
2022-08-24 10:42:06 +0900

    우희원 : 백준 10867 중복 빼고 정렬하기

--- week3/heeWon/test.py
+++ week3/heeWon/test.py
@@ -1,17 +1,5 @@
 n = int(input())
 
-def sum_num(inputs):
-    result = 0
-    for i in inputs:
-        if i.isdigit():
-            result+=int(i)
-    return result
+lst = list(set(map(int, input().split())))
 
-arr = []
-for i in range(n):
-    a = input()
-    arr.append(a)
-
-arr.sort(key = lambda x:(len(x), sum_num(x), x))
-for i in arr:
-    print(i)
+print(sorted(lst))
